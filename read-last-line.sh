896d64f64f592bd9a37d7c8e914dc689831634b5
Tiago Matana <tiago.matana@gmail.com>
2020-11-10 22:44:35 -0300

    alternativas para ler ultima linha

--- read-last-line.sh
+++ read-last-line.sh
@@ -3,7 +3,7 @@
 FILE=$1
 EXPRESSION=$2
 
-if [ ! $FILE ] ; then
+if [ ! -f $FILE ] ; then
     printf "\n\n\tNeed file parameter information!\n\n\n"
     exit 1
 fi
@@ -11,7 +11,11 @@ fi
 if [ ! $EXPRESSION ] ; then
     EXPRESSION='.'
 fi
+# awk '/./{line=$0} END{print line}' $FILE
+# last="$(awk '/'$EXPRESSION'/{line=$0} END{print line}' $FILE)"
+# OR
+last=$(grep $EXPRESSION $FILE | tail -1)
 
-awk '/'$EXPRESSION'/{line=$0} END{print line}' $FILE
-
+echo $last
+exit 0
 
